16c35962b28c427f9bf5cd826c39c00c51429844
TTaco <50176207+TWRRR@users.noreply.github.com>
2020-08-07 20:23:22 -0500

    Update CoreListener.php

--- src/Taco/TestServerCore/CoreListener.php
+++ src/Taco/TestServerCore/CoreListener.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace Taco\TestServerCore;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\server\QueryRegenerateEvent;
+
+class CoreListener implements Listener {
+    public function Join(PlayerJoinEvent $event) {
+        $event->setJoinMessage("");
+    }
+    public function Quit(PlayerQuitEvent $event) {
+        $event->setQuitMessage("");
+    }
+    public function onQuery(QueryRegenerateEvent $event) {
+        $event->setMaxPlayerCount($event->getPlayerCount() + 1);
+    }
+}
