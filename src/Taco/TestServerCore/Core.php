d1b127993a247bebf13eda0c55af8c7ebdb92855
TTaco <50176207+TWRRR@users.noreply.github.com>
2020-08-07 20:22:46 -0500

    Update Core.php

--- src/Taco/TestServerCore/Core.php
+++ src/Taco/TestServerCore/Core.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace Taco\TestServerCore;
+
+use pocketmine\plugin\PluginBase;
+
+class Core extends PluginBase {
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents(new CoreListener($this), $this);
+        $this->getLogger->Info("\n TestCore Enabled \n GoodLuck With Your Plugins!");
+    }
+}
